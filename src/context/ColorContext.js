eeb30bec804dfd3688858467dd995b003e090b6b
Ramesh Vijayaraghavan <rvramesh@gmail.com>
2022-07-25 13:21:32 +0000

    Added state change

--- src/context/ColorContext.js
+++ src/context/ColorContext.js
@@ -1,18 +1,18 @@
 import React, { useContext } from 'react'
 
-const ColorModeContext = React.createContext('light')
+const ColorModeContext = React.createContext({ mode: 'light', update: null })
 const ColorModeProvider = ColorModeContext.Provider
 
 const useColorMode = () => useContext(ColorModeContext);
 
-const ColoredApp = ({children}) => {
-    const [colorMode, setColorMode] = React.useState('light')
-
+const ColoredApp = ({ children }) => {
+  const [colorMode, setColorMode] = React.useState({ mode: 'light' })
+  const changeColor = (mode) => setColorMode({ ...colorMode, mode: mode })
   return (
-    <ColorModeProvider value={{ colorMode, update: setColorMode }}>
+    <ColorModeProvider value={{ colorMode, update: changeColor }}>
       {children}
     </ColorModeProvider>
   )
 }
 
-export { ColoredApp, useColorMode}
+export { ColoredApp, useColorMode }
--- src/context/test/ColorContext.spec.js
+++ src/context/test/ColorContext.spec.js
@@ -1,18 +1,26 @@
 import React from 'react'
-import {render, cleanup, renderHook} from '@testing-library/react'
+import { render, cleanup, renderHook, act } from '@testing-library/react'
 import { ColoredApp, useColorMode } from '../ColorContext'
 
 afterEach(cleanup)
 
 
-const renderComponent = ()=>{
-    return render(<ColoredApp />)
+const renderComponent = () => {
+  return render(<ColoredApp />)
 }
 it('renders without crashing', () => {
-  renderComponent() 
+  renderComponent()
   expect(ColoredApp).toBeDefined()
 })
+
 it('returns current color', () => {
-  const {result} = renderHook(() => useColorMode())
-  expect(result.current).toEqual('light')
+  const { result } = renderHook(() => useColorMode(), {
+    wrapper: ({ children }) => <ColoredApp>{children}</ColoredApp>
+  })
+
+  expect(result.current.colorMode.mode).toEqual('light')
+  act(() => {
+    result.current.update('dark')
+  })
+  expect(result.current.colorMode.mode).toEqual('dark')
 })
